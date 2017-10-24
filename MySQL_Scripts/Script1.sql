287ac9296f048ef33fc36c90474f111cfcaefdac
tehspyke <mr69@students.uwf.edu>
2017-10-24 16:39:06 -0500

    Added another file

--- MySQL_Scripts/Script1.sql
+++ MySQL_Scripts/Script1.sql
@@ -1,3 +1,29 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Oct 24, 2017 at 09:38 PM
+-- Server version: 5.7.19
+-- PHP Version: 5.6.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+--
+-- Database: `blackfeatherfestival`
+--
+CREATE DATABASE IF NOT EXISTS `blackfeatherfestival` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `blackfeatherfestival`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `first_name` blob NOT NULL,
