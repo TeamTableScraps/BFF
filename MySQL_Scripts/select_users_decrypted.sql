894ad3d6b520dcf59faa116401803089b16b7a66
tehspyke <mr69@students.uwf.edu>
2017-11-07 14:20:40 -0600

    Added JQuery and Colorbox libraries

---
+++ MySQL_Scripts/select_users_decrypted.sql
@@ -0,0 +1,6 @@
+SELECT 
+	AES_DECRYPT(first_name,'BFF') as first_name,
+	AES_DECRYPT(last_name,'BFF') as last_name,
+    AES_DECRYPT(email,'BFF') as email,
+    AES_DECRYPT(password,'BFF') as password
+FROM `users`
