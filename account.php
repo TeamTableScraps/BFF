0beadfeb70c80e144404d446b7a9d766e655ca14
tehspyke <mr69@students.uwf.edu>
2017-12-02 14:48:22 -0600

    Minor changes

--- account.php
+++ account.php
@@ -32,14 +32,14 @@ echo "
             <tr><td colspan='2' align='center' class='addr_label'>&nbsp;</td></tr>
             <tr>
                 <td class='addr_button_cell1' colspan='2'>
-                    <form action='TODO.php' method='post' class='popup'>
+                    <form action='TODO.php' method='post' class='popupForm'>
                     <input style='padding: 0px 0px;' class='button_modern varPadding' type='submit' name='submit' value='Edit Account'/>
                     </form>
                 </td>
             </tr>
             <tr>
 			    <td class='addr_button_cell2' colspan='2'>
-			        <form method='post' action='TODO.php' class='popup'>
+			        <form method='post' action='TODO.php' class='popupForm'>
                     <input style='padding: 0px 0px;' class='button_modern varPadding' type='submit' name='submit' value='Change Password'/>
                     </form>
                 </td>
@@ -54,13 +54,34 @@ echo "
             <tr class='addr_header_row'>
                 <td colspan='2' align='center' class='addr_header'>REGISTER AS A VENDOR</td>
             </tr>
-            <tr><td colspan='2' align='center' class='addr_label'>&nbsp;TODO</td></tr>
+            <tr>
+                <td class='addr_label'>Vendor Name:</td>
+                <td class='addr_label'></td>
+            </tr>
+            <tr>
+                <td class='addr_label'>Vendor Email:</td>
+                <td class='addr_label'></td>
+            </tr>
+            <tr>
+                <td class='addr_label'>Vendor Phone:</td>
+                <td class='addr_label'></td>
+            </tr>
             <tr><td colspan='2' align='center' class='addr_label'>&nbsp;</td></tr>
             <tr><td colspan='2' align='center' class='addr_label'>&nbsp;</td></tr>
-            <tr><td colspan='2' align='center' class='addr_label'>&nbsp;</td></tr>
-            <tr><td colspan='2' align='center' class='addr_label'>&nbsp;</td></tr>
-            <tr><td colspan='2' align='center' class='addr_label' style='height:55px;'>&nbsp;</td></tr>
-            <tr><td colspan='2' align='center' class='addr_label' style='height:46px;'>&nbsp;</td></tr>
+            <tr>
+			    <td class='addr_button_cell1' colspan='2'>
+			        <form method='post' action='TODO.php' class='popupForm'>
+                    <input style='padding: 0px 0px;' class='button_modern varPadding' type='submit' name='submit' value='Register as Vendor'/>
+                    </form>
+                </td>
+			</tr>
+            <tr>
+			    <td class='addr_button_cell2' colspan='2'>
+			        <form method='post' action='TODO.php' class='popupForm'>
+                    <input style='padding: 0px 0px;' class='button_modern varPadding' type='submit' name='submit' value='Reserve Booth'/>
+                    </form>
+                </td>
+			</tr>
         </table>
     </td>
     
@@ -70,13 +91,31 @@ echo "
             <tr class='addr_header_row'>
                 <td colspan='2' align='center' class='addr_header'>REGISTER AS A SPONSOR</td>
             </tr>
-            <tr><td colspan='2' align='center' class='addr_label'>&nbsp;TODO</td></tr>
+            <tr>
+                <td class='addr_label'>Sponsorship Level:</td>
+                <td class='addr_label'></td>
+            </tr>
+            <tr>
+                <td class='addr_label'>Years participated:</td>
+                <td class='addr_label'></td>
+            </tr>
             <tr><td colspan='2' align='center' class='addr_label'>&nbsp;</td></tr>
             <tr><td colspan='2' align='center' class='addr_label'>&nbsp;</td></tr>
             <tr><td colspan='2' align='center' class='addr_label'>&nbsp;</td></tr>
-            <tr><td colspan='2' align='center' class='addr_label'>&nbsp;</td></tr>
-            <tr><td colspan='2' align='center' class='addr_label' style='height:55px;'>&nbsp;</td></tr>
-            <tr><td colspan='2' align='center' class='addr_label' style='height:46px;'>&nbsp;</td></tr>
+            <tr>
+			    <td class='addr_button_cell1' colspan='2'>
+			        <form method='post' action='TODO.php' class='popupForm'>
+                    <input style='padding: 0px 0px;' class='button_modern varPadding' type='submit' name='submit' value='Register as Sponsor'/>
+                    </form>
+                </td>
+			</tr>
+            <tr>
+			    <td class='addr_button_cell2' colspan='2'>
+			        <form method='post' action='TODO.php' class='popupForm'>
+                    <input style='padding: 0px 0px;' class='button_modern varPadding' type='submit' name='submit' value='Make Sponsorship'/>
+                    </form>
+                </td>
+			</tr>
         </table>
     </td>
     
