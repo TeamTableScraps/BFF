2c0852e423735bc0957cfdbd643dc43066b1b21e
tehspyke <mr69@students.uwf.edu>
2017-12-02 21:57:47 -0600

    Moved stuff

--- account.php
+++ account.php
@@ -77,7 +77,7 @@ echo "
 			</tr>
             <tr>
 			    <td class='addr_button_cell2' colspan='2'>
-			        <form method='post' action='TODO.php' class='popupForm'>
+			        <form method='post' action='vendors.php' class=''>
                     <input style='padding: 0px 0px;' class='button_modern varPadding' type='submit' name='submit' value='Reserve Booth'/>
                     </form>
                 </td>
@@ -111,7 +111,7 @@ echo "
 			</tr>
             <tr>
 			    <td class='addr_button_cell2' colspan='2'>
-			        <form method='post' action='TODO.php' class='popupForm'>
+			        <form method='post' action='sponsors.php' class=''>
                     <input style='padding: 0px 0px;' class='button_modern varPadding' type='submit' name='submit' value='Make Sponsorship'/>
                     </form>
                 </td>
@@ -123,169 +123,6 @@ echo "
     </tr>
     </table>
 ";
-
-
-
-echo '
-    
-	<div id="vendors" class="container-fluid">
-        
-		<h2>Registered Vendors</h2>
-        
-		<table class="defaultbg fullwidth">
-        
-		<tr>
-            
-			<th class="info_table">Name</th>
-            
-			<th class="info_table">Vendor Type</th>
-            
-			<th class="info_table">Number of Year Participating</th>
-            
-			<th class="info_table">Email</th>
-            
-			<th class="info_table">Phone Number</th>
-        
-		</tr>    
-';
-
-$vendorQuery = "
-    SELECT
-        user_ID,
-        booth_ID,
-        years_active,
-        vendor_type
-    FROM vendors
-";
-$vendorName = "ERROR";
-$vendors = $MySQLi->query($vendorQuery);
-
-if($vendors != false){
-    if ($vendors->num_rows > 0) {
-        while($r = $vendors->fetch_array()){
-            $userQuery = "
-              SELECT 
-                AES_DECRYPT(first_name, '$aesKey') as first_name, 
-                AES_DECRYPT(last_name, '$aesKey') as last_name, 
-                AES_DECRYPT(email, '$aesKey') as email,
-                AES_DECRYPT(phone, '$aesKey') as phone 
-              FROM users WHERE user_ID = '".$r['user_ID']."' LIMIT 1";
-            $userResult = $MySQLi->query($userQuery);
-            if($userResult != false){
-                if ($userResult->num_rows > 0) {
-                    while($u = $userResult->fetch_array()){
-                        $vendorName = $u['first_name']." ".$u['last_name'];
-                        $vendorPhone = $u['phone'];
-                        $vendorEmail = $u['email'];
-                    }
-                }
-            }
-            else{
-                die("Query error");
-            }
-
-            echo "
-                <tr>
-                    <td class=\"info_table\">".$vendorName."</td>
-                    <td class=\"info_table\">".$r['vendor_type']."</td>
-                    <td class=\"info_table\">".$r['years_active']."</td>
-                    <td class=\"info_table\">$vendorEmail</td>
-                    <td class=\"info_table\">$vendorPhone</td>
-                </tr>
-            ";
-        }
-    }
-}else{
-    die("Query error");
-}
-
-echo'        
-        
-		</table>
-    
-	</div>
-    
-    
-	
-	<div id="sponsors" class="container-fluid bg-fade">
-        
-		<h2>Registered Sponsors</h2>
-        
-		<table class="defaultbg fullwidth">
-        
-		<tr>
-            
-			<th class="info_table">Name</th>
-            
-			<th class="info_table">Sponsorship Level</th>
-            
-			<th class="info_table">Number of Year Participating</th>
-            
-			<th class="info_table">Email</th>
-            
-			<th class="info_table">Phone Number</th>
-        
-		</tr>
-        
-		<tr>
-            
-';
-
-$sponsorQuery = "
-    SELECT
-        user_ID,
-        AES_DECRYPT(sponsorship_level, '$aesKey') as sponsorship_level,
-        years_active
-    FROM sponsors
-";
-$sponsorName = "ERROR";
-$sponsors = $MySQLi->query($sponsorQuery);
-
-if($sponsors != false){
-    if ($sponsors->num_rows > 0) {
-        while($r = $sponsors->fetch_array()){
-            $userQuery = "
-              SELECT 
-                AES_DECRYPT(first_name, '$aesKey') as first_name, 
-                AES_DECRYPT(last_name, '$aesKey') as last_name, 
-                AES_DECRYPT(email, '$aesKey') as email,
-                AES_DECRYPT(phone, '$aesKey') as phone 
-              FROM users WHERE user_ID = '".$r['user_ID']."' LIMIT 1";
-            $userResult = $MySQLi->query($userQuery);
-            if($userResult != false){
-                if ($userResult->num_rows > 0) {
-                    while($u = $userResult->fetch_array()){
-                        $sponsorName = $u['first_name']." ".$u['last_name'];
-                        $sponsorPhone = $u['phone'];
-                        $sponsorEmail = $u['email'];
-                    }
-                }
-            }
-            else{
-                die("Query error");
-            }
-
-            echo "
-                <tr>
-                    <td class=\"info_table\">".$sponsorName."</td>
-                    <td class=\"info_table\">".$r['sponsorship_level']."</td>
-                    <td class=\"info_table\">".$r['years_active']."</td>
-                    <td class=\"info_table\">$sponsorEmail</td>
-                    <td class=\"info_table\">$sponsorPhone</td>
-                </tr>
-            ";
-        }
-    }
-}else{
-    die("Query error");
-}
-
-echo'	</tr>
-        
-		</table>
-    
-	</div>
-';
 
 require_once('footer.php');
 
--- vendors.php
+++ vendors.php
@@ -3,5 +3,84 @@ require_once("header.php");
 
 echo "<img src='Images/Venue%20Map.jpg'>";
 
+echo '
+    
+	<div id="vendors" class="container-fluid">
+        
+		<h2>Registered Vendors</h2>
+        
+		<table class="defaultbg fullwidth">
+        
+		<tr>
+            
+			<th class="info_table">Name</th>
+            
+			<th class="info_table">Vendor Type</th>
+            
+			<th class="info_table">Number of Year Participating</th>
+            
+			<th class="info_table">Email</th>
+            
+			<th class="info_table">Phone Number</th>
+        
+		</tr>    
+';
+
+$vendorQuery = "
+    SELECT
+        user_ID,
+        booth_ID,
+        years_active,
+        vendor_type
+    FROM vendors
+";
+$vendorName = "ERROR";
+$vendors = $MySQLi->query($vendorQuery);
+
+if($vendors != false){
+    if ($vendors->num_rows > 0) {
+        while($r = $vendors->fetch_array()){
+            $userQuery = "
+              SELECT 
+                AES_DECRYPT(first_name, '$aesKey') as first_name, 
+                AES_DECRYPT(last_name, '$aesKey') as last_name, 
+                AES_DECRYPT(email, '$aesKey') as email,
+                AES_DECRYPT(phone, '$aesKey') as phone 
+              FROM users WHERE user_ID = '".$r['user_ID']."' LIMIT 1";
+            $userResult = $MySQLi->query($userQuery);
+            if($userResult != false){
+                if ($userResult->num_rows > 0) {
+                    while($u = $userResult->fetch_array()){
+                        $vendorName = $u['first_name']." ".$u['last_name'];
+                        $vendorPhone = $u['phone'];
+                        $vendorEmail = $u['email'];
+                    }
+                }
+            }
+            else{
+                die("Query error");
+            }
+
+            echo "
+                <tr>
+                    <td class=\"info_table\">".$vendorName."</td>
+                    <td class=\"info_table\">".$r['vendor_type']."</td>
+                    <td class=\"info_table\">".$r['years_active']."</td>
+                    <td class=\"info_table\">$vendorEmail</td>
+                    <td class=\"info_table\">$vendorPhone</td>
+                </tr>
+            ";
+        }
+    }
+}else{
+    die("Query error");
+}
+
+echo'        
+        
+		</table>
+    
+	</div>';
+
 require_once("footer.php");
 ?>
